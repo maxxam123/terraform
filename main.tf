62be6aa19b360a12f503244ca581dec156948a96
max <max@max.fritz.box>
2022-11-22 15:12:11 +0100

    10

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "proxmox_vm_qemu" "test_master" {
 }
 
 resource "proxmox_vm_qemu" "test_worker" {
-  count = 1
+  count = 2
   name = "worker-${count.index + 1}"
   target_node = var.proxmox_host
   clone = var.template_name
